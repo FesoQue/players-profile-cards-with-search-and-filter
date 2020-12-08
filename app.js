de53c41227c0d06bb2a3b24511cc32fc59ecff36
FesoQue <adefesoq@gmail.com>
2020-12-09 00:50:04 +0100

    made an  adjustment to the nav and searchbar

--- app.js
+++ app.js
@@ -368,63 +368,63 @@ const squad = [...managers, ...players]; // the spread method
 // **** SELECTORS ****
 const sectionCenter = document.querySelector(".section-center");
 const selectOption = document.querySelector("select");
-const nav = document.querySelector('nav');
-const button = document.querySelectorAll('.menu-btn');
-const scrollLinks = document.querySelectorAll('.scroll-links');
-const links = document.querySelector('.nav-links');
-const linksContainer = document.querySelector('.links-container');
-const openBtn = document.querySelector('.open-btn');
-const closeBtn = document.querySelector('.close-btn');
-const contact = document.querySelector('.contact-wrapper');
-const contactLink = document.getElementById('contact-link');
-const closeModal = document.querySelector('.close-modal');
+const nav = document.querySelector("nav");
+const button = document.querySelectorAll(".menu-btn");
+const scrollLinks = document.querySelectorAll(".scroll-links");
+const links = document.querySelector(".nav-links");
+const linksContainer = document.querySelector(".links-container");
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const contact = document.querySelector(".contact-wrapper");
+const contactLink = document.getElementById("contact-link");
+const closeModal = document.querySelector(".close-modal");
 
 // **** SET DATE DYNAMICALLY ****
-const date = document.getElementById('date');
+const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // **** MENU BUTTON (OPEN/CLOSE LINKS) ****
 button.forEach(function (btn) {
-  btn.addEventListener('click', () => {
-  const nav = btn.parentElement.parentElement.parentElement.parentElement;
+  btn.addEventListener("click", () => {
+    const nav = btn.parentElement.parentElement.parentElement.parentElement;
 
-  // **** condition ***
-  !(nav.classList.contains('show-links')) ? nav.classList.add('show-links') : nav.classList.remove('show-links');
+    // **** condition ***
+    !nav.classList.contains("show-links")
+      ? nav.classList.add("show-links")
+      : nav.classList.remove("show-links");
 
-// **** DYNAMICALLY SET THE HEIGHT OF THE NAV LINKS ****
-const linksHeight = links.getBoundingClientRect().height;
-const linksContainerHeight = linksContainer.getBoundingClientRect().height;
+    // **** DYNAMICALLY SET THE HEIGHT OF THE NAV LINKS ****
+    const linksHeight = links.getBoundingClientRect().height;
+    const linksContainerHeight = linksContainer.getBoundingClientRect().height;
 
-(linksContainerHeight === 0) ? linksContainer.style.height = `${linksHeight}px` : linksContainer.style.height = 0;
-  })
-})
+    linksContainerHeight === 0
+      ? (linksContainer.style.height = `${linksHeight}px`)
+      : (linksContainer.style.height = 0);
+  });
+});
 
 // **** COLLAPSE THE LINKS CONTAINER HEIGHT FOR SMALLER SCREEN WHEN THE ACTUAL LINKS ARE BEING CLICKED ****
-scrollLinks.forEach(links => {
-  links.addEventListener('click', (e) => {
+scrollLinks.forEach((links) => {
+  links.addEventListener("click", (e) => {
     linksContainer.style.height = 0;
-    //console.log(links)
     const nav = links.parentElement.parentElement.parentElement.parentElement;
-    //console.log(nav)
     const currentLink = e.currentTarget;
-    if (links === currentLink) {
-     nav.classList.add('close');
-    } else {
-      nav.classList.remove('close')
-    }
-  })
-})
+
+    links === currentLink
+      ? nav.classList.add("close")
+      : nav.classList.remove("close");
+  });
+});
 
 // **** DISPLAY CONTACT MODAL
-contactLink.addEventListener('click', () => {
-  //console.log('click click')
-  contact.classList.toggle('show-modal');
-})
+contactLink.addEventListener("click", () => {
+  contact.classList.toggle("show-modal");
+});
 
 // *** CLOSE CONTACT MODAL ****
-closeModal.addEventListener('click', () => {
-  contact.classList.remove('show-modal')
-})
+closeModal.addEventListener("click", () => {
+  contact.classList.remove("show-modal");
+});
 
 // **** LOAD ITEMS ****
 window.addEventListener("DOMContentLoaded", function () {
@@ -438,9 +438,13 @@ window.addEventListener("DOMContentLoaded", function () {
     // *** document.documentElement.scrollHeight - window.innerHeight *** the result will be total scrollable height of your document ****
     const scrolled = Math.floor(window.scrollY); //scroll values
     // **** condition for toplink ****
-    scrolled > 1000 ? bookMark.classList.add("displayBookmark") : bookMark.classList.remove("displayBookmark");
+    scrolled > 1000
+      ? bookMark.classList.add("displayBookmark")
+      : bookMark.classList.remove("displayBookmark");
     // **** condition for fixed navbar ****
-    scrolled > navHeight? nav.classList.add('fixed-nav') : nav.classList.remove('fixed-nav');
+    scrolled > navHeight
+      ? nav.classList.add("fixed-nav")
+      : nav.classList.remove("fixed-nav");
   });
 });
 
@@ -541,7 +545,7 @@ function debounce(func, wait = 250) {
 // **** SEARCHBAR EVENT HANDLER ****
 function searchHandler(players, event) {
   const name = event.target.value.toLowerCase();
-// **** searching through the players card ****
+  // **** searching through the players card ****
   const cards = players
     .filter((squadMember) => squadMember.name.toLowerCase().includes(name))
     .sort(
@@ -549,7 +553,7 @@ function searchHandler(players, event) {
         a.name.toLowerCase().indexOf(name) - b.name.toLowerCase().indexOf(name)
     );
   displayTeamCard([], cards);
-// **** searching through the coach card ****
+  // **** searching through the coach card ****
   const coachCard = managers
     .filter((squadMember) => squadMember.name.toLowerCase().includes(name))
     .sort(
